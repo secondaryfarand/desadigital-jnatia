6f4cfeaeb66b667b57cee4c2a24b88589bb14123
Second Farand <randomoke123@gmail.com>
2026-08-08 02:52:11 +0800

    banyaknya cobaan 2

---
+++ src/app/surat/page.tsx
@@ -0,0 +1,6 @@
+// import LandingPage from '@/src/features/landing/components/LandingPage';
+import LandingPage from '@/features/landing/components/LandingPage';
+
+export default function Home() {
+  return <LandingPage />;
+}   
